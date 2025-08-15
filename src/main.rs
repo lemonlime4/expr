47f3972ad63dd178b7f212e5e74246234c6ac060
lemonlime4 <71093251+lemonlime4@users.noreply.github.com>
2025-08-15 19:17:45 -0400

    better error messages

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 mod lex;
 mod parse;
 mod run;
-use std::{path::Path, sync::mpsc, time::Duration};
+use std::{io::Write, path::Path, sync::mpsc, time::Duration};
 
 use notify::{Event, RecursiveMode, Watcher, recommended_watcher};
 use notify_debouncer_full::{DebounceEventResult, new_debouncer, notify};
@@ -13,42 +13,50 @@ use crate::{
 };
 
 fn main() {
-    use io::Write;
-    use std::io;
-
     let (send, recv) = mpsc::channel();
     let mut debouncer = new_debouncer(Duration::from_millis(100), None, send).unwrap();
-    debouncer
+    if debouncer
         .watch("./input.txt", RecursiveMode::NonRecursive)
-        .unwrap();
+        .is_err()
+    {
+        println!("`input.txt` was not found in current directory");
+        return;
+    }
 
     // repl
     loop {
-        let mut input = std::fs::read_to_string(Path::new("./input.txt")).unwrap();
-        // let mut input = String::new();
-        // if let Err(error) = std::io::stdin().read_line(&mut input) {
-        //     println!("error: {error}")
-        // }
-
         println!("--------------------------------");
-        match parse(input.as_str()) {
-            Ok(items) => match Interpreter::new().run(items) {
-                Ok(results) => {
-                    for result in results {
-                        println!("{result}");
-                    }
-                }
-                Err(message) => {
-                    println!("-- {message} --");
-                }
-            },
-            Err(message) => {
-                println!("-- {message} --");
+
+        match std::fs::read_to_string(Path::new("./input.txt")) {
+            Ok(input) => {
+                run_and_print(input.as_str());
             }
-        }
-        println!();
+            Err(err) => {
+                println!("Couldn't read from input.txt");
+                println!("{err:?}")
+            }
+        };
         std::io::stdout().flush().unwrap();
 
         recv.recv();
     }
+}
+
+fn run_and_print(input: &str) {
+    match parse(input) {
+        Ok(items) => match Interpreter::new().run(items) {
+            Ok(results) => {
+                for result in results {
+                    println!("{result}");
+                }
+            }
+            Err(message) => {
+                println!("Error -- {message}");
+            }
+        },
+        Err(message) => {
+            println!("Parse error -- {message}");
+        }
+    }
+    println!();
 }
