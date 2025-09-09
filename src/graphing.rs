13c75800b0aa885f41acd0964578989d817d8e72
lemonlime4 <71093251+User2573@users.noreply.github.com>
2025-09-09 11:16:13 -0400

    adaptive sampling non-undefined functions

--- src/graphing.rs
+++ src/graphing.rs
@@ -10,26 +10,54 @@ pub fn sample_single_var_function(
     graph_to_window: impl Fn(Point) -> Point,
 ) -> BezPath {
     assert!(initial_segment_count >= 2);
-    let mut path = BezPath::new();
-    let mut new_segment = true;
-    // let mut points = Vec::new();
+    /// A point sorted by its finite x coordinate
+    // #[derive(PartialEq, PartialOrd, Clone, Copy)]
+    // struct Pt(f64, f64);
+    // impl Eq for Pt {}
+    // impl Ord for Pt {
+    //     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+    //         self.0.partial_cmp(&other.0).unwrap()
+    //     }
+    // }
+    let mut points = Vec::new();
     for i in 0..=initial_segment_count {
         let x = {
             let t = i as f64 / initial_segment_count as f64;
             xmin * (1.0 - t) + xmax * t
         };
-        let y = f(x);
-        let point = graph_to_window(Point { x, y });
-        if y.is_finite() {
-            if new_segment {
-                path.move_to(point);
-                new_segment = false;
-            } else {
-                path.line_to(point);
+        points.push(Point::new(x, f(x)));
+    }
+    let min_angle: f64 = 175.0;
+    let mut need_resampling = Vec::new();
+    for _ in 0..5 {
+        let mut resampled = false;
+        need_resampling.clear();
+        need_resampling.resize(points.len() - 1, false);
+        for (i, (&p1, &p2, &p3)) in points.iter().tuple_windows().enumerate() {
+            if (p1 - p2).dot(p3 - p2) > min_angle.to_radians().cos() {
+                need_resampling[i] = true;
+                need_resampling[i + 1] = true;
+                resampled = true;
             }
-        } else {
-            new_segment = true;
         }
+        // if !resampled {
+        //     break;
+        // }
+
+        let mut offset = 0;
+        for (i, &need_resampling) in need_resampling.iter().enumerate() {
+            if need_resampling {
+                let x = points[i + offset].x.midpoint(points[i + offset + 1].x);
+                points.insert(i + offset + 1, Point::new(x, f(x)));
+                offset += 1;
+            }
+        }
+    }
+
+    let mut path = BezPath::new();
+    path.move_to(graph_to_window(points[0]));
+    for p in &points[1..] {
+        path.line_to(graph_to_window(*p));
     }
     path
 }
